6113368e041448e000a6f1ff74a60d1700ea730f
DerStudent <strolsu@googlemail.com>
2017-06-12 11:31:51 +0200

    DBZugriff mit preparedStatement erstellt für die gesamte auto liste

--- src/java/dao/DBZugriff.java
+++ src/java/dao/DBZugriff.java
@@ -5,10 +5,41 @@
  */
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import dto.Auto;
+
 /**
  *
  * @author Denis
  */
 public class DBZugriff {
-    
+
+    public ArrayList<Auto> getAutos(Connection conn) throws SQLException {
+        ArrayList<Auto> autoliste = new ArrayList<>();
+        PreparedStatement pstm = conn.prepareStatement("SELECT * FROM Auto");
+        ResultSet rs = pstm.executeQuery();
+
+        try {
+            while (rs.next()) {
+                Auto auto = new Auto();
+                auto.setFahrgestellnummer(rs.getString(1));
+                auto.setHalter(rs.getNString(2));
+                auto.setHersteller(rs.getNString(3));
+                auto.setKennzeichen(rs.getString(4));
+                auto.setkW(rs.getInt(5));
+                auto.setModelname(rs.getString(6));
+                autoliste.add(auto);
+            }
+        } catch (SQLException e) {
+            
+        }
+        return autoliste;
+
+    }
+
 }
