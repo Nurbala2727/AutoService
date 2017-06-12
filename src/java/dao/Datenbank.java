b9febaaf9158f4ca913e8569c048a504635ea186
DerStudent <strolsu@googlemail.com>
2017-06-12 11:09:39 +0200

    datenbankverbindung erstellt

--- src/java/dao/Datenbank.java
+++ src/java/dao/Datenbank.java
@@ -5,10 +5,25 @@
  */
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author Denis
  */
 public class Datenbank {
     
+    private Connection conn;
+    private String url = "jdbc:mysql://127.0.0.1:3306/autoservice";
+    
+    public Connection getConnection()throws Exception{
+        
+        try {
+            conn = DriverManager.getConnection(url,"roor","");
+            return conn;
+        } catch (Exception e) {
+            throw e;
+        }
+    }
 }
