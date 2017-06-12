4c083fd67ccad1d5b262fc75c01feceb13a4d87e
DerStudent <strolsu@googlemail.com>
2017-06-12 10:35:47 +0200

    Attribute der Klasse Auto hinzugefügt

--- src/java/dto/Auto.java
+++ src/java/dto/Auto.java
@@ -11,4 +11,75 @@ package dto;
  */
 public class Auto {
     
+    private String hersteller;
+    private String modelname;
+    private String fahrgestellnummer; 
+    private String kennzeichen;
+    private String halter;
+    private int kW;
+    
+    public Auto(){
+        
+    }
+
+    public Auto(String hersteller, String modelname, String fahrgestellnummer, String kennzeichen, String halter, int kW) {
+        this.hersteller = hersteller;
+        this.modelname = modelname;
+        this.fahrgestellnummer = fahrgestellnummer;
+        this.kennzeichen = kennzeichen;
+        this.halter = halter;
+        this.kW = kW;
+    }
+
+    public String getHersteller() {
+        return hersteller;
+    }
+
+    public void setHersteller(String hersteller) {
+        this.hersteller = hersteller;
+    }
+
+    public String getModelname() {
+        return modelname;
+    }
+
+    public void setModelname(String modelname) {
+        this.modelname = modelname;
+    }
+
+    public String getFahrgestellnummer() {
+        return fahrgestellnummer;
+    }
+
+    public void setFahrgestellnummer(String fahrgestellnummer) {
+        this.fahrgestellnummer = fahrgestellnummer;
+    }
+
+    public String getKennzeichen() {
+        return kennzeichen;
+    }
+
+    public void setKennzeichen(String kennzeichen) {
+        this.kennzeichen = kennzeichen;
+    }
+
+    public String getHalter() {
+        return halter;
+    }
+
+    public void setHalter(String halter) {
+        this.halter = halter;
+    }
+
+    public int getkW() {
+        return kW;
+    }
+
+    public void setkW(int kW) {
+        this.kW = kW;
+    }
+    
+    
+    
+    
 }
